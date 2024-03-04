b55f1bda36f0d853fe5d407f02cf17f60fbcd4ea
Tluong1238 <thuyluong.ap@gmail.com>
2024-03-04 23:09:54 +0700

    Create Project

---
+++ JAVA_MVC/src/java_mvc/JAVA_MVC.java
@@ -0,0 +1,16 @@
+package java_mvc;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class JAVA_MVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
